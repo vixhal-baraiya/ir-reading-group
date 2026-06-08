e616e52867a2f885a43abf9f75b9d9ce953e4547
Vishal (TheVixhal) <vixhalbaraiya@gmail.com>
2026-06-08 17:08:46 +0530

    Update and rename src/content/config.ts to src/content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,7 +1,8 @@
 import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
 
 const dbInternals = defineCollection({
-  type: 'content',
+  loader: glob({ pattern: '**/*.md', base: './src/content/db-internals' }),
   schema: z.object({
     title: z.string(),
     chapter: z.number(),
